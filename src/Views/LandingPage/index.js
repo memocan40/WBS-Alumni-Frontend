4cd340d4f0b853281e96f7bcfe7124e9e28e3ad8
David Surina <david.surina22@gmail.com>
2021-03-01 13:15:53 +0100

    added basic styling to the landing page

--- src/Views/LandingPage/index.js
+++ src/Views/LandingPage/index.js
@@ -4,8 +4,8 @@ export default function LandingPage(){
     return(
         <div className="landing-page-wrapper">
             <div className="landing-page-content-wrapper">
-                <h3>welcome to</h3>
-                <h1>WBS Alumni</h1>
+                <h3 className="landing-page-subheading">welcome to</h3>
+                <h1 className="landing-page-heading">WBS <span class="lighter-font">Alumni</span></h1>
                 <div className="landing-page-btn-wrapper">
                     <button class="log-in-btn">Log in</button>
                     <button class="register-btn">Register</button>
