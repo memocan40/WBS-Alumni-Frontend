e194bc0d34613d4b3c808cbb87f0a4d321bb7b1f
David Surina <david.surina22@gmail.com>
2021-03-10 23:30:00 +0100

    added media queries for student card and corrected normal styling

--- src/Components/StudentCard/index.js
+++ src/Components/StudentCard/index.js
@@ -3,17 +3,17 @@ import {Link} from "react-router-dom";
 
 import './style.css';
 
-export default function Usercard({ name, batch, id }) {
+export default function Studentcard({ name, batch, id }) {
   const { t, i18n } = useTranslation();
   
   return (
-      <div className="user-card">
-          <div className="user-card-img-wrapper">
-            <img className="user-card-img" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" alt="Profilepic" />
+      <div className="student-card">
+          <div className="student-card-img-wrapper">
+            <img className="student-card-img" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" alt="Profilepic" />
           </div>
-          <h1 id="user-card-name">{t('name.label')} {name}</h1>
-          <p class="user-card-desc">{t('batch.label')} {batch}</p>
-          <Link to={`/studentprofile/${id}`}> <button class="user-card-btn">{t('contact.label')}</button></Link>
+          <h1 className="student-card-name">{t('name.label')} {name}</h1>
+          <p className="student-card-desc">{t('batch.label')} {batch}</p>
+          <Link to={`/studentprofile/${id}`} className="student-card-btn">{t('contact.label')}</Link>
       </div>
   );
 }
