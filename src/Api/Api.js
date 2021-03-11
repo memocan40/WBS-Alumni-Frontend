7a3e96688697ac7a15882e414bd290be8089ce66
olkhon <alexander.stapel@gmail.com>
2021-03-11 13:09:47 +0100

    api response msg new user

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,62 +1,76 @@
 import axios from "axios";
 
-const baseURL="http://localhost:3000";
+const baseURL="https://hidden-shelf-31461.herokuapp.com/";
+// const baseURL = "http://localhost:3000/";
 
+const Api = {
+  getAllUsers: async () => {
+    try {
+      const response = await axios.get(`${baseURL}users`);
+      if (response.data) {
+        return response.data.data;
+      } else {
+        console.log("no user data");
+        return [];
+      }
+    } catch (e) {
+      console.error("getAllUsers: ", e);
+      return [];
+    }
+  },
 
-const Api={
-    getAllUsers: async()=>{
-        try{
-            const response = await axios.get(`${baseURL}users`);
-            if(response.data) {
-                return response.data.data;
-            } else {
-                console.log("no user data");
-                return [];
-            }
+  updateUserbyID: async (obj) => {
+    try {
+      const response = await axios.put(`${baseURL}users/update/${obj.id}`, obj);
+      if (response) {
+        console.log(response);
+        return response;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      console.error("updateUserbyID:", e);
+      return [];
+    }
+  },
 
-        } catch(e) {
-            console.error("getAllUsers: ", e);
-            return [];
+  logIn: (requestBody) => {
+    axios.post(`${baseURL}users/login`, requestBody, {
+      headers: { "Access-Control-Allow-Origin": "http://localhost:3000" },
+    });
+  },
+  getInterests: async () => {
+    try {
+      const response = await axios.get(`${baseURL}interests`);
+      if (response) {
+        console.log(response);
+        return response.data.data;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      console.error("getInterests:", e);
+      return [];
+    }
+  },
+
+  getUserbyID: (id) => {
+    axios.get(`${baseURL}users/${id}`);
+  },
+  createNewuser: (requestBody) => {
+    axios
+      .post(`${baseURL}users/register`, requestBody, {
+        headers: { "Access-Control-Allow-Origin": "http://localhost:3000" },
+      })
+      .then(
+        (response) => {
+          console.log(response.data);
+        },
+        (error) => {
+          console.log(error);
         }
-    },
-
-    updateUserbyID: async (obj)=>{
-        try{
-            const response = await axios.put(`${baseURL}users/update/${obj.id}`, obj);
-            if(response) {
-                console.log(response)
-                return response;
-            } else {
-                return[];
-            }
-        }catch(e) {
-            console.error("updateUserbyID:", e);
-            return [];
-        }
-    },
-
-    logIn:(requestBody)=>{axios.post(`${baseURL}users/login`,requestBody,  {
-        headers:{'Access-Control-Allow-Origin' : "http://localhost:3000" }})},
-    getInterests: async() => {
-        try{
-            const response = await axios.get(`${baseURL}interests`);
-            if(response) {
-                console.log(response);
-                return response.data.data;
-            }else {
-                return [];
-            }
-        }
-        catch(e) {
-            console.error("getInterests:", e);
-            return [];
-        }
-    },
-
-    getUserbyID:(id)=>{axios.get(`${baseURL}users/${id}`)},
-    createNewuser:(requestBody)=>{axios.post(`${baseURL}users/register`,requestBody,
-    {
-        headers:{'Access-Control-Allow-Origin' : "http://localhost:3000" }})}
-}
+      );
+  },
+};
 
 export default Api;
