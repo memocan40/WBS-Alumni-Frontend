0bfacfd2758dea3efa4f4ec072ab35228bb572f5
David Surina <david.surina22@gmail.com>
2021-03-12 13:43:33 +0100

    corrected searchbar state variable and added axios call for search

--- src/Components/Searchbar/index.js
+++ src/Components/Searchbar/index.js
@@ -1,45 +1,36 @@
-import React, { useState } from 'react';
-
 import "./style.css";
 
-export default function Searchbar({searchOption,inputValue, batches, cities, workstatus}) {
+export default function Searchbar({searchOption, inputValue, batches, cities, workstatus}) {
 
-    let [inputvalue,setinputvalue]=useState("");
-
-    const batchesOptions = batches.map(el => {
-        return <option className="reach-out-option" key={el.name} value={el.name}>{el.name}</option>
-    })
-    const citiesOptions = cities.map(el => {
-        return <option className="reach-out-option" key={el.name} value={el.name}>{el.name}</option>
-    })
-    const workstatusOptions = workstatus.map(el => {
-        return <option className="reach-out-option" key={el.name} value={el.name}>{el.name}</option>
-    })
-    
-    if(inputvalue){inputValue(inputvalue)}
     if(searchOption == "workstatus") {
-
+        const workstatusOptions = workstatus.map(el => {
+            return <option className="reach-out-option" key={el.name} value={el.name}>{el.name}</option>
+        })
         return (
-            <select className="reach-out-select" onChange={(e)=> (setinputvalue(e.target.value))}>
+            <select className="reach-out-select" onChange={(e)=> (inputValue(e.target.value))}>
                {workstatusOptions}
             </select>
         )
     } else if (searchOption == "batch" && batches) {
-
+        const batchesOptions = batches.map(el => {
+            return <option className="reach-out-option" key={el.name} value={el.name}>{el.name}</option>
+        })
         return (
-            <select className="reach-out-select" onChange={(e)=> (setinputvalue(e.target.value))}>
+            <select className="reach-out-select" onChange={(e)=> (inputValue(e.target.value))}>
                {batchesOptions}
             </select>
         )
     } else if (searchOption == "interests") {
 
         return (
-            <input className="reach-out-input" type="text" onChange={(e)=>{setinputvalue(e.target.value)}} ></input>
+            <input className="reach-out-input" type="text" onChange={(e)=>{inputValue(e.target.value)}} ></input>
         )
     } else if (searchOption == "city" && cities) {
-
+        const citiesOptions = cities.map(el => {
+            return <option className="reach-out-option" key={el.name} value={el.name}>{el.name}</option>
+        })
         return (
-            <select className="reach-out-select" onChange={(e)=> (setinputvalue(e.target.value))}>
+            <select className="reach-out-select" onChange={(e)=> (inputValue(e.target.value))}>
                {citiesOptions}
             </select>
         ) 
--- src/Views/Reachout/index.js
+++ src/Views/Reachout/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import Header from '../../Components/Header';
 import Loader from "../../Components/Loader";
@@ -10,26 +11,39 @@ import './style.css';
 
 export default function ReachOut({students, batches, cities, workstatus}) {
 
-  const [dropdownSelected, setDropdownSelected] = useState('batch');
-  let [childValue,setchildValue]                =useState();
+  const [typeOfSearch, setTypeOfSearch] = useState('batch');
+  const [childValue,setchildValue]=useState("");
   const [filteredStudents, setFilteredStudents] = useState({...students});
   
-  console.log(dropdownSelected);
+  console.log(typeOfSearch);
   console.log(childValue);
+
+  const searchController = async () => {
+    //e.preventDefault();
+    try{
+      const response = await axios.get(`https://hidden-shelf-31461.herokuapp.com/users/${typeOfSearch}/${childValue}`)
+      if(response) {
+        console.log(response)
+      }
+    }catch(e){
+      console.error(e);
+    }
+  }
+
 
   if(filteredStudents) {
   return (
     <>
       <Header />
       <div className="search-bar-wrapper">
-        <select className="reach-out-select" onChange={(e)=> setDropdownSelected(e.target.value)}>
+        <select className="reach-out-select" onChange={(e)=> setTypeOfSearch(e.target.value)}>
           <option className="reach-out-option" value="batch">Batch</option>
           <option className="reach-out-option" value="city">City</option>
           <option className="reach-out-option" value="workstatus">Workstatus</option>
           <option className="reach-out-option" value="interests">Interests</option>
         </select>
-        <Searchbar searchOption={dropdownSelected} inputValue={(valueFromChild)=>{setchildValue(valueFromChild)}} batches={batches} cities={cities} workstatus={workstatus} />
-        <button className="search-bar-btn" type="submit">Search</button>
+        <Searchbar searchOption={typeOfSearch} inputValue={setchildValue} batches={batches} cities={cities} workstatus={workstatus} />
+        <button className="search-bar-btn" onClick={searchController}>Search</button>
       </div>
       <div className="search-grid">
         {students ? students.map((filteredStudents) => {
