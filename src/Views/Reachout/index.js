bf0740255be31d632dab3e1e11d73f50e3db663f
David Surina <david.surina22@gmail.com>
2021-03-14 14:59:10 +0100

    solved issue with user dropdown where first item needs to be selected twice and issue with rendering of search

--- src/Views/Reachout/index.js
+++ src/Views/Reachout/index.js
@@ -13,10 +13,14 @@ export default function ReachOut({students, batches, cities, workstatus}) {
 
   const [typeOfSearch, setTypeOfSearch] = useState('batch');
   const [childValue,setchildValue]=useState("");
-  const [filteredStudents, setFilteredStudents] = useState({...students});
+  const [filteredStudents, setFilteredStudents] = useState(students);
   
   console.log(typeOfSearch);
   console.log(childValue);
+  console.log(filteredStudents);
+
+
+
 
   const searchController = async () => {
     //e.preventDefault();
@@ -24,7 +28,9 @@ export default function ReachOut({students, batches, cities, workstatus}) {
       const response = await axios.get(`https://hidden-shelf-31461.herokuapp.com/users/${typeOfSearch}/${childValue}`)
       if(response) {
         console.log(response)
-        setFilteredStudents({...response.data.data});
+        setFilteredStudents(response.data.data);
+        console.log("render")
+        console.log(filteredStudents)
       }
     }catch(e){
       console.error(e);
@@ -47,9 +53,9 @@ export default function ReachOut({students, batches, cities, workstatus}) {
         <button className="search-bar-btn" onClick={searchController}>Search</button>
       </div>
       <div className="search-grid">
-        {students ? students.map((filteredStudents) => {
-          return <StudentCard name={`${filteredStudents.first_name} ${filteredStudents.last_name}`} batch={filteredStudents.batch} id={filteredStudents.id}/>
-        }) : "no data"}
+        {filteredStudents.map((filteredStudent) => {
+          return <StudentCard name={`${filteredStudent.first_name} ${filteredStudent.last_name}`} batch={filteredStudent.batch} id={filteredStudent.id}/>
+        })}
       </div>
       <Footer />
     </>
