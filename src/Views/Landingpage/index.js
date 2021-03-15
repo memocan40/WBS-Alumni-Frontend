b592c26190840dc5d49b0dfcab19f1abebd79d74
memo40 <memocann4040@gmail.com>
2021-03-15 16:27:04 +0100

    .

--- src/Views/Landingpage/index.js
+++ src/Views/Landingpage/index.js
@@ -16,7 +16,7 @@ export default function LandingPageView() {
       <div className="landing-page-content-wrapper">
         
         <h1 className="landing-page-heading">
-          <img className="logo" src="https://images-ext-1.discordapp.net/external/FaoDRqQ9jGvnDT5nh2rMzKCCOhwOwg08pjggeLuhkrg/%3Fwidth%3D1440%26height%3D379/https/media.discordapp.net/attachments/781868539765063711/814832419270164510/standard_colour_cutout_text_icon.png?width=1025&height=270"></img> <span className="lighter-font">Alumni</span>
+          <img className="logo1" src="https://images-ext-1.discordapp.net/external/FaoDRqQ9jGvnDT5nh2rMzKCCOhwOwg08pjggeLuhkrg/%3Fwidth%3D1440%26height%3D379/https/media.discordapp.net/attachments/781868539765063711/814832419270164510/standard_colour_cutout_text_icon.png?width=1025&height=270"></img> <span className="lighter-font">Alumni</span>
         </h1>
         <div className="landing-page-btn-wrapper">
           <Link to="/login" class="log-in-btn">
