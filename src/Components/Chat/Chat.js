ee70e6beba16faac45273c5b5e69a197458b5ba2
memo40 <memocann4040@gmail.com>
2021-03-16 17:21:56 +0100

    addet chat close button

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -4,84 +4,102 @@ import socketClient from 'socket.io-client';
 
 import { useState, useEffect } from 'react';
 
-export default function Chat({username}) {
-  let [inputvalue, setinputvalue] = useState('');
-  let[name,setname]=useState(username);
-  let [messages, setmessages] = useState([]);
-  let [mouseclick, setmouseclick] = useState();
+export default function Chat({ username }) {
+	let [inputvalue, setinputvalue] = useState('');
+	let [name, setname] = useState(username);
+	let [messages, setmessages] = useState([]);
+	let [mouseclick, setmouseclick] = useState();
+  let[chatwindow,setchatwindow]=useState("Chat");
 
-  const SERVER = 'http://localhost:3005/';
-  const socket = socketClient(SERVER);
+	const SERVER = 'http://localhost:3005/';
+	const socket = socketClient(SERVER);
 
-  useEffect(() => {
-    socket.on('chat message', function (msg) {
-      messages.push(msg);
-      setmessages([...messages]);
-    });
-  }, []);
+	useEffect(() => {
+		socket.on('chat message', function (msg) {
+			messages.push(msg);
+			setmessages([...messages]);
+		});
+	}, []);
 
-  let textfield = document.getElementById('input');
-  if(name===undefined){setname("anonymous")};
+	let textfield = document.getElementById('input');
+	if (name === undefined) {
+		setname('anonymous');
+	}
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+	const submitHandler = (e) => {
+		e.preventDefault();
 
-    const msg = { inputvalue, name };
-    socket.emit('chat message', msg);
+		const msg = { inputvalue, name };
+		socket.emit('chat message', msg);
 
-    setmessages([...messages, msg]);
+		setmessages([...messages, msg]);
 
-    textfield.value = '';
-  };
+		textfield.value = '';
+	};
 
-  function updateScroll() {
-    var element = document.getElementById('messages');
-    element.scrollTop = element.scrollHeight;
+  const Chatbutton=()=>{
+    if(chatwindow==="Chat"){
+      setchatwindow("Chatclosed")
+      console.log("chat closed")
+    }
+    else if(chatwindow==="Chatclosed"){
+      setchatwindow("Chat")
+    }
   }
 
- 
-    if (mouseclick === false) {
+	function updateScroll() {
+		var element = document.getElementById('messages');
+		element.scrollTop = element.scrollHeight;
+	}
+
+	if (mouseclick === false) {
 		updateScroll();
 	}
 
-
-  console.log(messages);
-  return (
-    <div
-      className="Chat"
-      onMouseDown={() => {
-        setmouseclick(true);
-      }}
-      onMouseUp={() => {
-        setmouseclick(false);
-      }}
-      onMouseOver={() => {
-        setmouseclick(false);
-      }}>
-      <ul id="messages">
-        <div id="messagesli">
-          {messages.map((iteration, index) => {
-            return (
-              <li id="username">
-                {iteration.name}
-                <li className="input" id="messageli">
-                  {iteration.inputvalue}
-                </li>
-              </li>
-            );
-          })}
-        </div>
-      </ul>
-      <form onSubmit={submitHandler} id="form" action="">
-        <input
-          id="input"
-          autocomplete="off"
-          onChange={(event) => {
-            setinputvalue(event.target.value);
-          }}
-        />
-        <button id="button">Send</button>
-      </form>
-    </div>
-  );
+	console.log(messages);
+	return (
+		<div>
+		
+			<button id="chatwindowbutton" onClick={Chatbutton}>
+				Chat
+			</button>
+			<div
+				className={chatwindow}
+				onMouseDown={() => {
+					setmouseclick(true);
+				}}
+				onMouseUp={() => {
+					setmouseclick(false);
+				}}
+				onMouseOver={() => {
+					setmouseclick(false);
+				}}
+			>
+				<ul id="messages">
+					<div id="messagesli">
+						{messages.map((iteration, index) => {
+							return (
+								<li id="username">
+									{iteration.name}
+									<li className="input" id="messageli">
+										{iteration.inputvalue}
+									</li>
+								</li>
+							);
+						})}
+					</div>
+				</ul>
+				<form onSubmit={submitHandler} id="form" action="">
+					<input
+						id="input"
+						autocomplete="off"
+						onChange={(event) => {
+							setinputvalue(event.target.value);
+						}}
+					/>
+					<button id="button">Send</button>
+				</form>
+			</div>
+		</div>
+	);
 }
