18ab5c41b92417b501e8f22d98d787c9e122af4c
Elie <eliegebransoued@gmail.com>
2021-04-07 16:40:30 +0200

    putting the profile text inside of the logo in the profile drop down

--- src/Components/Userdropdown/index.js
+++ src/Components/Userdropdown/index.js
@@ -1,19 +1,19 @@
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useTranslation } from 'react-i18next';
-import {useState} from 'react';
-import { Link, useHistory  } from 'react-router-dom';
-import axios from 'axios';
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
-import Loader from '../Loader';
+import Loader from "../Loader";
 
-import './style.css';
+import "./style.css";
 
 export default function UserDropdown() {
   const { t, i18n } = useTranslation();
   const history = useHistory();
 
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
   const logout = async (e) => {
     e.preventDefault();
@@ -23,34 +23,35 @@ export default function UserDropdown() {
         `https://hidden-shelf-31461.herokuapp.com/users/logout`
       );
       if (response) {
-        history.push('/');
+        history.push("/");
       }
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-  if(!loading) {
-  return (
-    <DropdownButton
-      id="dropdown-basic"
-      variant="secondary"
-      className="user-dropdown"
-      title="&#9660;">
-      <Dropdown.Item>
-        <Link className="dropdown-link" to="/profile">
-          <i class="far fa-user-circle"></i>
-          {t('profile.label')}
-        </Link>
-      </Dropdown.Item>
-      <Dropdown.Item>
-        <div onClick={logout} className="dropdown-link">
-          <i class="fas fa-sign-out-alt"></i>
-          {t('logout.label')}
-        </div>
-      </Dropdown.Item>
-    </DropdownButton>
-  );} else {
-    return <Loader />
+  if (!loading) {
+    return (
+      <DropdownButton
+        id="dropdown-basic"
+        variant="secondary"
+        className="user-dropdown"
+        title="&#9660;"
+      >
+        <Dropdown.Item>
+          <Link className="dropdown-link" to="/profile">
+            <i class="far fa-user-circle">{t("profile.label")}</i>
+          </Link>
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <div onClick={logout} className="dropdown-link">
+            <i class="fas fa-sign-out-alt"></i>
+            {t("logout.label")}
+          </div>
+        </Dropdown.Item>
+      </DropdownButton>
+    );
+  } else {
+    return <Loader />;
   }
 }
