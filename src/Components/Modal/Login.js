bb2e1bd725d0f16c615fd333ccc8f2659af228d5
Elie <eliegebransoued@gmail.com>
2021-05-11 10:45:08 +0200

    hard coding the deployed url again

--- src/Components/Modal/Login.js
+++ src/Components/Modal/Login.js
@@ -27,7 +27,7 @@ export default function Login({ setLoggedUser, loggedUser, setToken }) {
     setloading(true);
     try {
       const response = await axios.post(
-        [`http://localhost:3000/users/login`],
+        [`https://hidden-shelf-31461.herokuapp.com/users/login`],
         data
       );
 
